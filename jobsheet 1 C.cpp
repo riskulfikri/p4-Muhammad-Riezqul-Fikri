4bef0e92e2ba3bd653d901411bfaccaf298c24b2
Rizkul0.e <64573745+riskulfikri@users.noreply.github.com>
2020-05-12 04:14:43 -0700

    Create jobsheet 1 C.cpp
    membuat bilangan array

---
+++ jobsheet 1 C.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int my_array[5] = {1,9,0,9};
+	printf("nama : M Riezqul Fikri");
+	printf("\nNim : F1B019099");
+	printf("\nKelompok : 20\n");
+	
+	printf("\nData ke-1= %d",my_array[0]);
+	printf("\nData ke-2= %d",my_array[1]);
+	printf("\nData ke-3= %d",my_array[2]);
+	printf("\nData ke-4= %d",my_array[3]);
+	printf("\nData ke-5= %d",my_array[4]);
+}
